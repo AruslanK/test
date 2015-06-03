509a194395bca3b137bc7421d226e0f7c0fb378f
ruslan <ruslan.kryvosheiev@LWO1-DHP-F09815.synapse.com>
2015-06-03 14:58:05 +0300

    pass driver to listener

--- KandyTest/src/test/java/uitl/ListenerOne.java
+++ KandyTest/src/test/java/uitl/ListenerOne.java
@@ -1,95 +1,28 @@
 package uitl;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
+import RegistrationTest.AbstractTest;
 import ru.yandex.qatools.allure.annotations.Attachment;
 import ru.yandex.qatools.allure.annotations.Step;
 
-import RegistrationTest.AbstractTest;
 
 public class ListenerOne extends TestListenerAdapter {
-	
-	protected ByteArrayOutputStream ous = null;
-    protected InputStream ios = null;
-    
-	 @Step("Hi, I'm step in your testng listener")
+		
+	    @Step("Hi, I'm step in your testng listener")
 	    @Override
-	    public void onTestFailure(ITestResult testResult) {
-		 
-		         takeScreenShotOnFailure(testResult);
+	    public void onTestFailure(ITestResult tr) {
+	    	Object currentClass = tr.getInstance();
+	        WebDriver driver = ((AbstractTest) currentClass).getDriver();
+	        createAttachment(driver);
 	    }
 
-	
-	@Attachment(value = "Page screenshot", type = "image/png")
-	public byte [] takeScreenShotOnFailure(ITestResult testResult) {
-		Object currentClass = testResult.getInstance();
-        WebDriver driver = ((AbstractTest)currentClass).getDriver();
-        return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);		
-
-//        if (driver != null)
-//        {
-//
-//        	if (testResult.getStatus() == ITestResult.FAILURE) {
-//    			System.out.println("щас скриншотну"+testResult.getStatus());
-//    			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-//    			try {
-//    		        byte[] buffer = new byte[4096];
-//    		        ous = new ByteArrayOutputStream();
-//    		        try {
-//    					ios = new FileInputStream(scrFile);
-//    				} catch (FileNotFoundException e) {
-//    					// TODO Auto-generated catch block
-//    					e.printStackTrace();
-//    				}
-//    		        int read = 0;
-//    		        try {
-//    					while ( (read = ios.read(buffer)) != -1 ) {
-//    					    ous.write(buffer, 0, read);
-//    					}
-//    				} catch (IOException e) {
-//    					// TODO Auto-generated catch block
-//    					e.printStackTrace();
-//    				}
-//    		    } finally { 
-//    		        try {
-//    		             if ( ous != null ) 
-//    		                 ous.close();
-//    		        } catch ( IOException e) {
-//    		        }
-//
-//    		        try {
-//    		             if ( ios != null ) 
-//    		                  ios.close();
-//    		        } catch ( IOException e) {
-//    		        }
-//    		    }
-//    		    
-//    			try {
-//    				FileUtils.copyFile(scrFile, new File("D:\\testScreenShot.jpg"));
-//    			} catch (IOException e) {
-//    				// TODO Auto-generated catch block
-//    				e.printStackTrace();
-//    			}
-//    			
-//    		}
-//
-//           //etc.
-//        }
-//		
-//		return ous.toByteArray();
-		
-	} 
-	
-}
+	    @Attachment("Hi, I'm attachment in your testng listener")
+	    public byte[]  createAttachment(WebDriver driver) {
+		    return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);			    
+		}
+	}
